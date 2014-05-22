ebf0c353f99c0f75abbad0375bca9cb037fc5a9f
Björn Rathjen <bjoern.rathjen@gmail.com>
2014-05-22 15:18:01 +0200

    kein plan warum u05 nun nicht geht

--- u05/mdue05/GrafikDemoProgramm.java
+++ u05/mdue05/GrafikDemoProgramm.java
@@ -6,6 +6,31 @@ import java.awt.*;
 import java.awt.event.*;
 //import java.awt.geom.Point2D;
 
+public class GrafikDemoProgramm {
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Grafik-Testprogramm");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
+        JPanel panel = new TestPanel();
+
+        frame.getContentPane().add(panel);
+        //Display the window.
+        frame.setVisible(true);
+    }
+
+    
+    
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
+
 @SuppressWarnings("serial")
 class TestPanel extends JPanel implements MouseListener {
     int x = 10;
@@ -67,28 +92,4 @@ class TestPanel extends JPanel implements MouseListener {
     
 //    TODO linie zeichnen
     
-}
-
-public class GrafikDemoProgramm {
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Grafik-Testprogramm");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,400);
-        JPanel panel = new TestPanel();
-
-        frame.getContentPane().add(panel);
-        //Display the window.
-        frame.setVisible(true);
-    }
-
-    
-    
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
 }
