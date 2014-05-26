bf226ecf4a802b336a29f13a550c2e18371db994
Björn Rathjen <bjoern.rathjen@gmail.com>
2014-05-26 14:03:38 +0200

    kein plan du wolltest es so

--- u05/myue05/GrafikDemoProgramm.java
+++ u05/myue05/GrafikDemoProgramm.java
@@ -55,38 +55,82 @@ class TestPanel extends JPanel implements MouseListener {
     // Bresenham Algorithmus //
     // /////////////////////////
 
+    // soll mit übergabe die punkte entsprechend einzeichnen
+    public void drawpoint() {
+
+    }
+
     public void mypaint(Point p1, Point p2, Graphics g) {
 
         int x_start, x_end, y_start, y_end;
+        double m;
 
-        if (p1.x < p2.x) {
-            x_start = p1.x;
-            y_start = p1.y;
-            x_end = p2.x;
-            if (p1.y > p2.y) {
-                for (int i = x_start; i < x_end; i += 4) {
-                    int y;
-                    y = y_start + Math.round(i * (p2.y - p1.y) / (p2.x - p1.x));
-                    g.fillRect(i, y, 4, 4);
-                }
-            }else {
-                for (int i = x_start; i < x_end; i += 4) {
-                    int y;
-                    y = y_start - Math.round(i * (p2.y - p1.y) / (p2.x - p1.x));
-                    g.fillRect(i,y, 4, 4);
-                }
+        if (p1.x - p2.x != 0) {
+            m = (p1.y - p2.y) / (p1.x - p2.x);
+        }else {
+            m=1;
+        }
+
+        // Unterscheidung nach der Steigung
+        if (m < -1) {
+            // p1 über p2
+            if (p1.x < p2.x) {
+                x_start = p1.x;
+                x_end = p2.x;
+                y_start = p1.y;
+                y_end = p2.y;
+            } else {
+                // p2 über p1
+                x_start = p2.x;
+                x_end = p1.x;
+                y_start = p2.y;
+                y_end = p1.y;
             }
-        } else {
-            x_start = p2.x;
-            y_start = p2.y;
-            x_end = p1.x;
-            for (int i = x_start; i < x_end; i += 4) {
-                int y;
-                y = y_start + Math.round(i * (p2.y - p1.y) / (p2.x - p1.x));
-                g.fillRect(i, y_start + y, 4, 4);
+            g.drawLine(x_start, y_start, x_end, y_end);
+            for (int i = 0; i < y_end; y = y - 4) {
+                x = (int) Math.round(x_start - i * (y_start - i) / m);
+                g.fillRect(x, y_start - i, 4, 4);
+            }
+
+        } else if (m <= -1 && m < 0) {
+            // p1 über p2
+            if (p1.x < p2.x) {
+
+            } else {
+                // p2 über p1
+
+            }
+        } else if (m <= 0 && m < 1) {
+            // p1 über p2
+            if (p1.x < p2.x) {
+
+            } else {
+                // p2 über p1
+
+            }
+        } else if (m <= 1) {
+            // p1 über p2
+            if (p1.x < p2.x) {
+
+            } else {
+                // p2 über p1
 
             }
         }
+
+        /*
+         * if (p1.x < p2.x) { x_start = p1.x; y_start = p1.y; x_end = p2.x; if
+         * (p1.y > p2.y) { for (int i = x_start; i < x_end; i += 4) { int y; y =
+         * y_start + Math.round((i-x_start) * (p2.y - p1.y) / (p2.x - p1.x));
+         * g.fillRect(i, y, 4, 4); } }else { for (int i = x_start; i < x_end; i
+         * += 4) { int y; y = y_start - Math.round(i * (p2.y - p1.y) / (p2.x -
+         * p1.x)); g.fillRect(i,y, 4, 4); } } } else { x_start = p2.x; y_start =
+         * p2.y; x_end = p1.x; for (int i = x_start; i < x_end; i += 4) { int y;
+         * y = y_start + Math.round((i-x_start) * (p2.y - p1.y) / (p2.x -
+         * p1.x)); g.fillRect(i, y_start + y, 4, 4);
+         * 
+         * } }
+         */
     }
 
     // ////////////////////
